446129214ffaa0f44c6e60870a7823c7a140d8b3
arisupriatna14 <arisupriatna703@gmail.com>
2018-10-23 14:54:51 +0700

    add route login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/users')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router
+  .post('/register', register)
+  .post('/login', login)
 
 module.exports = router;
