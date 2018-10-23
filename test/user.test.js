05f722f25e18b214cab784fde2e564fa1f8ed37e
arisupriatna14 <arisupriatna703@gmail.com>
2018-10-23 14:54:16 +0700

    add testing user

---
+++ test/user.test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+describe('Register', function() {
+  it ('POST /register should return object new user', function(done) {
+    done();
+  })
+  it ('POST /login should return object user login', function(done) {
+    done();
+  })
+})
